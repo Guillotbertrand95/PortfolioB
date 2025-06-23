0a10767b2e06fc5379cc4c9c9b291962473e8858
guillot bertrand <guillotbertrand95@gmail.com>
2025-06-23 16:16:43 +0200

    moddif navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,21 @@ import "../styles/layout/_navbar.scss";
 
 function Navbar() {
 	return (
-		<nav className="navbar">
-			<ul className="navbar__list">
-				<li className="navbar__item">
-					<NavLink to="/">Accueil</NavLink>
-				</li>
-				<li className="navbar__item">
-					<NavLink to="/projets">Projets</NavLink>
-				</li>
-				<li className="navbar__item">
-					<NavLink to="/contact">Contact</NavLink>
-				</li>
-			</ul>
-		</nav>
+		<>
+			<nav className="navbar">
+				<ul className="navbar__list">
+					<li className="navbar__item">
+						<NavLink to="/">Accueil</NavLink>
+					</li>
+					<li className="navbar__item">
+						<NavLink to="/projets">Projets</NavLink>
+					</li>
+					<li className="navbar__item">
+						<NavLink to="/contact">Contact</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
 }
 
